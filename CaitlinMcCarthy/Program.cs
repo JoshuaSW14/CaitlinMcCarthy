bedc7ac6e1d85a048ac87eae93fe41e246cb2eb7
Joshua Symons-Webb <Joshua.Symons-Webb2@ontario.ca>
2024-01-15 11:51:09 -0500

    Add project files.

---
+++ CaitlinMcCarthy/Program.cs
@@ -0,0 +1,33 @@
+using CaitlinMcCarthy.Client.Pages;
+using CaitlinMcCarthy.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(CaitlinMcCarthy.Client._Imports).Assembly);
+
+app.Run();

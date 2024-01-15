2bf30ef04578806ae9d3cee4fcfef603cac08f95
Joshua Symons-Webb <Joshua.Symons-Webb2@ontario.ca>
2024-01-15 12:53:23 -0500

    Update

---
+++ CaitlinMcCarthy.Client/Models/DateIdeaGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace CaitlinMcCarthy.Client.Models;
+public class DateIdeaGenerator
+{
+    private static readonly List<string> DateIdeas = new List<string>
+    {
+        "Watch a movie under the stars.",
+        "Have a picnic in the park.",
+        "Visit a museum or art gallery.",
+        "Go for a scenic hike.",
+        "Try a new restaurant in town.",
+        // Add more ideas here
+    };
+
+    private static readonly Random Random = new Random();
+
+    public string GetRandomDateIdea()
+    {
+        int index = Random.Next(DateIdeas.Count);
+        return DateIdeas[index];
+    }
+}
---
+++ CaitlinMcCarthy.Client/Models/MessageGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace CaitlinMcCarthy.Client.Models;
+public class MessageGenerator
+{
+    private static readonly List<string> Messages = new List<string>
+    {
+        "I hope you have a good day!",
+        "Thinking of you always makes me smile.",
+        "You mean the world to me.",
+        "I'm so lucky to have you in my life.",
+        "Every moment with you is precious.",
+        // Add more messages here
+    };
+
+    public string GetRandomMessage()
+    {
+        return Messages[new Random().Next(0, Messages.Count)];
+    }
+}
